fe55a5ec03e4907e5488b3dd7492ccef0eb81ccf
DUCTHSE61994\DoitsuAdmin <thd1152018@gmail.com>
2017-03-14 18:08:37 +0700

    Update for sang sam set

--- BiddingCar/TestSQLEntityFramework/Program.cs
+++ BiddingCar/TestSQLEntityFramework/Program.cs
@@ -10,25 +10,7 @@ namespace TestSQLEntityFramework
     {
         static void Main(string[] args)
         {
-            using (var db = new AssignmentDBEntities())
-            {
-                var collection = db.Bidies.ToList();
-                foreach (var item in collection)
-                {
-                    Console.WriteLine(String.Format("{0,-10}{1,-10}{2,-10}{3,10}"
-                        ,item.Id,item.IdAccount,item.IdProduct,item.Price));
-                }
 
-                var collection2 = db.Bidies.Join(
-                        db.Products,
-                        b => b.
-                    );
-
-                foreach (var item in collection2)
-                {
-                    Console.WriteLine("{0,-10}");
-                }
-            }
 
         }
     }
